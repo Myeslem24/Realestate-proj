fef2c717bd26ee16861028acaac8738429a0cd5d
Mohamed Yeslem <medyislim1@gmail.com>
2025-06-23 12:20:39 +0000

    Set DEBUG=False and configured ALLOWED_HOSTS for Render

--- realestate/settings.py
+++ realestate/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kx-r5zux4oe=sy^lwa+)9!4z_59t@g8=jvxuz24!hfd5@fotid'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://realestate-proj.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -124,14 +124,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # اختياري: زيادة الحد الأقصى لحجم رفع الفيديو
 DATA_UPLOAD_MAX_MEMORY_SIZE = 52428800  # 50 MB
 FILE_UPLOAD_MAX_MEMORY_SIZE = 52428800
--- realestate/urls.py
+++ realestate/urls.py
@@ -15,5 +15,5 @@ urlpatterns = [
 ]
 
 # مهم جداً لعرض الصور المخزنة في MEDIA_ROOT
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
